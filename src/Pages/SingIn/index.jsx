deb2fe2e87a522924dab68788c8832d156536bb5
diogo-kappaun <diogohkappaun@gmail.com>
2023-12-30 21:35:08 -0300

    :sparkles: feat: Creating SignIn page

---
+++ src/Pages/SingIn/index.jsx
@@ -0,0 +1,10 @@
+export function SignIn() {
+  return (
+    <div className="">
+      <h1 className="text-4xl font-bold text-zinc-400">RocketMovies</h1>
+      <p className="text-sm text-zinc-500">
+        Aplicação para acompanhar tudo que assistir.
+      </p>
+    </div>
+  )
+}
