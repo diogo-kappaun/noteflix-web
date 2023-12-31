db55c1d87eb49c1769a4a8675023e8ffbdfc45c7
diogo-kappaun <diogohkappaun@gmail.com>
2023-12-30 21:34:45 -0300

    :sparkles: feat: Creating global div

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { SignIn } from './Pages/SingIn'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="font-poppins h-screen bg-zinc-950">
+      <SignIn />
+    </div>
   </React.StrictMode>,
 )
