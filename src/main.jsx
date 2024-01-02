448bcbff75eff2ac0feb510b07e7c061a6172961
diogo-kappaun <diogohkappaun@gmail.com>
2024-01-02 11:49:31 -0300

    :construction: Main interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignIn } from './Pages/SingIn'
+import { Start } from './Pages/Start'
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="h-screen bg-zinc-950 font-poppins">
-      <SignIn />
+    <div className="bg-kashmir-50 dark:bg-kashmir-950 h-screen font-poppins">
+      <Start />
     </div>
   </React.StrictMode>,
 )
