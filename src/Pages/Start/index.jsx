40d3cccbd38d79f08eed8bbfc4d018c8374b4910
diogo-kappaun <diogohkappaun@gmail.com>
2024-01-02 15:03:39 -0300

    :sparkles: feat: Starting Page

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,24 +1,26 @@
 import { ChevronRight } from 'lucide-react'
+import { Button } from '../../Components/Button'
 
 export function Start() {
   return (
-    <div className="grid-rows-app grid h-screen items-center justify-center">
-      <header className="flex max-w-[1440px] justify-center p-4">
-        <h1 className="font-anton text-3xl text-kashmir-950 dark:text-kashmir-50">
+    <div className="grid-rows-app mx-auto grid h-screen max-w-[1440px] items-center justify-center">
+      <header className="pt-6">
+        <h1 className="text-center font-anton text-3xl text-kashmir-950 dark:text-kashmir-50">
           ROCKETMOVIES
         </h1>
       </header>
-      <main className="flex max-w-[1440px] flex-1 flex-col items-center justify-center space-y-4 px-6 text-center">
-        <h2 className="text-3xl font-bold text-kashmir-950 dark:text-kashmir-50">
+
+      <main className="flex max-w-[1440px] flex-1 flex-col items-center justify-center space-y-6 px-6 text-center">
+        <h2 className="text-3xl font-bold text-kashmir-950 md:text-4xl dark:text-kashmir-50">
           Filmes, séries e muito mais, sem limites
         </h2>
-        <p className="text-md text-kashmir-900 dark:text-kashmir-200">
+        <p className="text-md text-kashmir-900 md:text-lg dark:text-kashmir-200">
           Explore o mundo do cinema em minutos, resumos envolventes dos seus
           filmes favoritos, tudo em um só lugar!
         </p>
-        <button className=" flex items-center rounded-lg border-2 border-kashmir-900 bg-transparent p-3 text-lg font-semibold shadow-sm transition-colors duration-300 hover:bg-kashmir-900 hover:text-kashmir-100 dark:border-kashmir-50 dark:text-kashmir-50 dark:hover:border-kashmir-100 dark:hover:bg-kashmir-100 dark:hover:text-kashmir-900">
+        <Button className="border-kashmir-900 bg-transparent hover:bg-kashmir-900 hover:text-kashmir-100 dark:border-kashmir-100 dark:text-kashmir-100 dark:hover:border-kashmir-100 dark:hover:bg-kashmir-100 dark:hover:text-kashmir-900">
           Vamos lá <ChevronRight />
-        </button>
+        </Button>
       </main>
     </div>
   )
