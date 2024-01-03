299ec080ae9277bb1a6ecfaf2424ea66a0c2f4a9
diogo-kappaun <diogohkappaun@gmail.com>
2024-01-02 21:52:50 -0300

    🧩 component: Brand

---
+++ src/Components/Brand.jsx
@@ -0,0 +1,11 @@
+import { cn } from '../Utils/cn'
+
+export function Brand({ className }) {
+  return (
+    <header className={cn('w-full bg-zinc-950 py-6 shadow-sm', className)}>
+      <h1 className="font-anton tracking-wide text-zinc-950 sm:text-3xl dark:text-zinc-100">
+        RocketMovies
+      </h1>
+    </header>
+  )
+}
