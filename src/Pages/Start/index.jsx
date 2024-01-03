8cf14da16fb2fee61e0b40df2c73d17c3d4e4341
diogo-kappaun <diogohkappaun@gmail.com>
2024-01-03 11:03:30 -0300

    :lipstick: fix: Adjusting the style of the starting page.

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -4,18 +4,18 @@ import { Brand } from '../../Components/Brand'
 
 export function Start() {
   return (
-    <div className="mx-auto grid h-screen max-w-[1440px] grid-rows-app items-center justify-center">
-      <Brand />
+    <div className="grid h-screen w-full grid-rows-app bg-gradient-to-t from-zinc-950 from-30% to-zinc-800">
+      <Brand className="px-6 text-xl md:px-12" />
 
       <main className="flex max-w-[1440px] flex-1 flex-col items-center justify-center space-y-6 px-6 text-center">
-        <h2 className="text-kashmir-950 dark:text-kashmir-50 text-3xl font-bold md:text-4xl">
+        <h2 className="text-3xl font-bold text-zinc-950 md:text-4xl dark:text-zinc-50">
           Filmes, séries e muito mais, sem limites
         </h2>
-        <p className="text-md text-kashmir-900 dark:text-kashmir-200 md:text-lg">
+        <p className="text-md text-zinc-900 md:text-lg dark:text-zinc-200">
           Explore o mundo do cinema em minutos, resumos envolventes dos seus
           filmes favoritos, tudo em um só lugar!
         </p>
-        <Button className="border-kashmir-900 hover:bg-kashmir-900 hover:text-kashmir-100 dark:border-kashmir-100 dark:text-kashmir-100 dark:hover:border-kashmir-100 dark:hover:bg-kashmir-100 dark:hover:text-kashmir-900 bg-transparent">
+        <Button className="mt-4 flex justify-center rounded-full bg-indigo-500 px-8 text-base text-zinc-100 transition-transform hover:scale-105">
           Vamos lá <ChevronRight />
         </Button>
       </main>
