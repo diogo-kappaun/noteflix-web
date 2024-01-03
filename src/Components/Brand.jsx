ec49b0530777c9b2aabc5dd1d92878fbdc82d491
diogo-kappaun <diogohkappaun@gmail.com>
2024-01-03 10:18:24 -0300

    :recycle: refactor: Adjusting responsiveness settings

--- src/Components/Brand.jsx
+++ src/Components/Brand.jsx
@@ -3,7 +3,7 @@ import { cn } from '../Utils/cn'
 export function Brand({ className }) {
   return (
     <header className={cn('w-full bg-zinc-950 py-6 shadow-sm', className)}>
-      <h1 className="font-anton tracking-wide text-zinc-950 sm:text-3xl dark:text-zinc-100">
+      <h1 className="font-anton tracking-wide text-zinc-950 md:text-3xl dark:text-zinc-100">
         RocketMovies
       </h1>
     </header>
