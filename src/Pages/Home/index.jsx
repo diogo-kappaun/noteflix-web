026ca821c2eb3c01160ca9127be812f9d1e4df50
diogo-kappaun <diogohkappaun@gmail.com>
2024-01-05 14:26:17 -0300

    :sparkles: feat: Home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import { Header } from '../../Components/Header'
 import { Input } from '../../Components/Input'
+import { Button } from '../../Components/Button'
+import { FiPlus } from 'react-icons/fi'
+import { MovieItem } from '../../Components/Home/MovieItem'
 
 export function Home() {
   return (
-    <div className="h-screen w-full bg-zinc-950">
+    <div className="w-full pb-12 text-zinc-100">
       <Header />
-      <div className="mx-auto mt-6 w-page px-6">
+
+      <div className="mx-auto mt-6 w-page space-y-6 px-6">
         <form className="flex" action="">
           <Input
             type="text"
@@ -13,7 +17,28 @@ export function Home() {
             className="w-full"
           />
         </form>
-        <h2>Meus filmes</h2>
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl md:text-3xl">Meus filmes</h2>
+          <Button className="flex items-center justify-center gap-2 bg-zinc-100 px-2">
+            <FiPlus className="h-4 w-4" />
+            Novo filme
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-6 overflow-auto">
+          <MovieItem
+            title="Caminhos Cruzados"
+            text="Em um mundo pós-apocalíptico, duas almas solitárias, Eli e Maya, cruzam seus caminhos em busca de um santuário legendário. Eli é um sobrevivente resiliente, mestre na arte da sobrevivência e guarda de um livro sagrado que pode conter a chave para a salvação da humanidade."
+            rating="4"
+            tags="Pós-apocalíptico"
+          />
+          <MovieItem
+            title="Caminhos Cruzados"
+            text="Em um mundo pós-apocalíptico, duas almas solitárias, Eli e Maya, cruzam seus caminhos em busca de um santuário legendário. Eli é um sobrevivente resiliente, mestre na arte da sobrevivência e guarda de um livro sagrado que pode conter a chave para a salvação da humanidade."
+            rating="4"
+            tags="Pós-apocalíptico"
+          />
+        </div>
       </div>
     </div>
   )
