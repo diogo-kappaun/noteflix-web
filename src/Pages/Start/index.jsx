a363188aecca859d02dbc213c491b12d48c4543d
diogo-kappaun <diogohkappaun@gmail.com>
2024-03-04 20:55:21 -0300

    style: Fix main alignment

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,13 +1,13 @@
 import { ChevronRight } from 'lucide-react'
-import { Button } from '../../Components/Button'
 import { Brand } from '../../Components/Brand'
+import { Button } from '../../Components/Button'
 
 export function Start() {
   return (
     <div className="grid h-screen w-full grid-rows-app bg-gradient-to-t from-zinc-950 from-30% to-zinc-800">
       <Brand className="px-6 text-xl md:px-12" />
 
-      <main className="flex max-w-[1440px] flex-1 flex-col items-center justify-center space-y-6 px-6 text-center">
+      <main className="mx-auto flex max-w-[1440px] flex-1 flex-col items-center justify-center space-y-6 px-6 text-center">
         <h2 className="text-3xl font-bold text-zinc-100 md:text-4xl">
           Filmes, séries e muito mais, sem limites
         </h2>
