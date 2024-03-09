75ad5d3bbf1cd5183e96b523e303cfde5240e597
diogo-kappaun <diogohkappaun@gmail.com>
2024-03-09 17:07:14 -0300

    fix: Home route address

--- src/Routes/app.routes.jsx
+++ src/Routes/app.routes.jsx
@@ -1,16 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import { Home } from '../Pages/Home'
-import { Profile } from '../Pages/Profile'
-import { MoviePreview } from '../Pages/MoviePreview'
 import { CreateMovie } from '../Pages/CreateMovie'
-import { Start } from '../Pages/Start'
+import { Home } from '../Pages/Home'
+import { MoviePreview } from '../Pages/MoviePreview'
+import { Profile } from '../Pages/Profile'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Start />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/preview" element={<MoviePreview />} />
       <Route path="/create" element={<CreateMovie />} />
