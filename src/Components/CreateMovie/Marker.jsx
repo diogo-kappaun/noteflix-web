3f11c05a692fb14b8c8861ae0f7813c96b9e0328
diogo-kappaun <diogohkappaun@gmail.com>
2024-03-17 22:39:33 -0300

    remove: Destroy marker component

--- src/Components/CreateMovie/Marker.jsx
+++
@@ -1,9 +0,0 @@
-import { X } from 'lucide-react'
-
-export function Marker({ title }) {
-  return (
-    <span className="flex w-max items-center gap-2 rounded-sm+ border border-zinc-700 px-2 py-1 text-sm text-zinc-100">
-      {title} <X className="h-4 w-4" />
-    </span>
-  )
-}
